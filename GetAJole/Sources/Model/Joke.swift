d779a09fb03cb0d7283ae895e6aae37a0ce366b9
BorisKiselev <hugh.dancy@icloud.com>
2023-03-13 20:27:12 +0700

    Create model.

---
+++ GetAJole/Sources/Model/Joke.swift
@@ -0,0 +1,40 @@
+//
+//  Joke.swift
+//  GetAJole
+//
+//  Created by Борис Киселев on 13.03.2023.
+//
+
+import Foundation
+import Realm
+import RealmSwift
+
+@objcMembers final class Joke: Object, Decodable {
+    
+    dynamic var setup: String = ""
+    dynamic var punchline: String = ""
+
+    enum CodingKeys: String, CodingKey {
+        case setup
+        case punchline
+    }
+    
+    required init(from decoder: Decoder) throws {
+        
+        let container = try! decoder.container(keyedBy: CodingKeys.self)
+        
+        setup = try container.decode(String.self, forKey: .setup)
+        punchline = try container.decode(String.self, forKey: .punchline)
+        
+        super.init()
+    }
+    
+    override static func primaryKey() -> String? {
+        return "setup"
+    }
+    
+   override required init() {
+            super.init()
+        }
+    
+}
