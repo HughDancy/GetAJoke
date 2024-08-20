8c95ad90071f630a5c1b7946864894f4948d73ef
BorisKiselev <hugh.dancy@icloud.com>
2024-08-20 11:18:55 +0700

    Add MainScreenDataBaseManger + Protocol.

--- GetAJole/Sources/Scenes/MainScreen/Controller/MainViewController.swift
+++ GetAJole/Sources/Scenes/MainScreen/Controller/MainViewController.swift
@@ -10,14 +10,18 @@ import Alamofire
 
 class MainViewController: UIViewController {
     
+    // MARK: - Properties
+    private var setup = ""
+    private var punch = ""
+
     // MARK: - Subview's
     private var mainView: MainView? {
         guard isViewLoaded else { return nil }
         return view as? MainView
     }
     
-    lazy var dataBaseManager = DBManager()
-    
+    lazy var dataBaseManager: DataBaseSavingProtocol = MainScreenDataBaseManager()
+
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,9 +35,8 @@ class MainViewController: UIViewController {
         let request = AF.request("https://official-joke-api.appspot.com/jokes/random")
         request.responseDecodable(of: Joke.self) { data in
             guard let randomJoke = data.value else { return }
-            
-            self.dataBaseManager.setup = randomJoke.setup
-            self.dataBaseManager.punch = randomJoke.punchline
+            self.setup = randomJoke.setup
+            self.punch = randomJoke.punchline
             
             DispatchQueue.main.async {
                 self.mainView?.setupLabel.setTyping(text: "\(randomJoke.setup) \n \(randomJoke.punchline)")
@@ -42,7 +45,7 @@ class MainViewController: UIViewController {
     }
     
     @objc func saveJoke() {
-        dataBaseManager.save()
+        dataBaseManager.saveJoke(setup: self.setup, punch: self.punch)
     }
 }
 
